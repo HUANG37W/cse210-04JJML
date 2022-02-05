8ab20d1e9f4235c38f8f219811c5dc3c65d7f988
jnounna <97972572+jnounna@users.noreply.github.com>
2022-02-05 12:12:01 -0700

    game folder and word file

---
+++ unit03-jumper/game/word.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace unit03_jumper
+{
+    class word
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Michael was here!");
+            
+            Console.WriteLine("Howdy Doodily.");
+        }
+    }
+}
