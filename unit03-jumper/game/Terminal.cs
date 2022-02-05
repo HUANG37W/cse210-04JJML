5da35093decc5a8249216a9be555b4bb4a571ec5
jnounna <97972572+jnounna@users.noreply.github.com>
2022-02-05 12:45:29 -0700

    terminal update

--- unit03-jumper/game/Terminal.cs
+++ unit03-jumper/game/Terminal.cs
@@ -2,15 +2,41 @@ using System;
 
 namespace unit03_jumper
 {
-    class Terminal
+        /// <summary>
+    /// <para>A service that handles terminal operations.</para>
+    /// <para>
+    /// The responsibility of a Terminal is to provide input and output operations for the 
+    /// terminal.
+    /// </para>
+    /// </summary>
+    public class Terminal
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Constructs a new instance of Terminal.
+        /// </summary>
+        public Terminal()
         {
-            Console.WriteLine("Hello World!");
+            //constructor
+        }
 
-            Console.WriteLine("Michael was here!");
-            
-            Console.WriteLine("Howdy Doodily.");
+        /// <summary>
+        /// Gets text input from the terminal. Directs the user with the given prompt.
+        /// </summary>
+        /// <param name="prompt">The given prompt.</param>
+        /// <returns>Inputted text.</returns>
+        public string ReadText(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Displays the given text on the terminal. 
+        /// </summary>
+        /// <param name="text">The given text.</param>
+        public void WriteText(string text)
+        {
+            Console.WriteLine(text);
         }
     }
 }
