323737057d98c68f9910c68aa860550decdde3fe
Lisa Child <gustavus05@gmail.com>
2022-02-05 13:12:39 -0700

    changed director

--- unit03-jumper/Program.cs
+++ unit03-jumper/Program.cs
@@ -1,16 +1,21 @@
-﻿using System;
+﻿using Unit03.Game;
+
 
 namespace unit03_jumper
 {
+    /// <summary>
+    /// The program's entry point.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Starts the program using the given arguments.
+        /// </summary>
+        /// <param name="args">The given arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            Console.WriteLine("Michael was here!");
-            
-            Console.WriteLine("Howdy Doodily.");
+            Director director = new Director();
+            director.StartGame();
         }
     }
 }
