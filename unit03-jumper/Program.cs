b90ae7ab453b3984925f217a45081f4cdc65490d
jnounna <97972572+jnounna@users.noreply.github.com>
2022-02-05 13:27:16 -0700

    merge fixed

--- unit03-jumper/Program.cs
+++ unit03-jumper/Program.cs
@@ -14,18 +14,9 @@ namespace unit03_jumper
         /// <param name="args">The given arguments.</param>
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("Hello World!");
-
-            Console.WriteLine("Michael was here!");
-            
-            Console.WriteLine("Howdy Doodily.");
-
-            
-=======
             Director director = new Director();
             director.StartGame();
->>>>>>> 0013b5ada12b62cbe65ef2ed069a9a6fb72d240b
+
         }
     }
 }
