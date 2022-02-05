b18bc286400b32b41380b8dda2c40d1122847fc7
Michael Knudsen <mikeman825@gmail.com>
2022-02-05 12:15:15 -0700

    Adding the intial version of Director class

---
+++ unit03-jumper/game/Director.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace unit03_jumper
+{
+    class Director
+    {
+        public void SetupGame()
+        {
+            //stub
+        }
+
+        public void StartGame()
+        {
+            //stub
+        }
+
+        public void DoInput()
+        {
+            //stub
+        }
+
+        public void DoUpdate()
+        {
+            //stub
+        }
+
+        public void DoOutput()
+        {
+            //stub
+        }
+    }
+}
