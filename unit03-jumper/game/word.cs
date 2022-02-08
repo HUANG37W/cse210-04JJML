9bfa1b7f425e4fa9c89689f97c73ccb41017e461
Lisa Child <gustavus05@gmail.com>
2022-02-08 09:42:17 -0700

    trying to merge

--- unit03-jumper/game/word.cs
+++ unit03-jumper/game/word.cs
@@ -4,15 +4,8 @@ namespace unit03_jumper
 {
     public class Word 
     {
-<<<<<<< HEAD
-        public Word();
-        {
-            //instance constructor
-        }
-=======
 
         public Terminal terminal = new Terminal();
->>>>>>> 030c179012f52afb8886c993caba80ecb9ca93eb
         private string[] LotrWords = new string[5]
         {
             "sauron", "frodo", "gollum", "gandalf", "legolas"
