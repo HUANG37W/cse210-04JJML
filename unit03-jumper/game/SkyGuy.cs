39fe2ab8a75be18a9a42ff931c4234e70e793532
Lisa Child <gustavus05@gmail.com>
2022-02-08 09:55:36 -0700

    new skyguy

--- unit03-jumper/game/SkyGuy.cs
+++ unit03-jumper/game/SkyGuy.cs
@@ -5,42 +5,23 @@ namespace unit03_jumper
 {
     class SkyGuy
     {
-        static void Main(string[] args)
-        {
 
-            List<string> parachute = AddNumbers();
-            {
-                PrintGuy(parachute);
-            }
-
-        static List<string> AddNumbers()
+         private string[] parachute = new string[8]
         {
-            List<string> parachute = new List<string>();
-            for (int i = 1; i< 7; i++)
+            " ____", " /   \\", " _____", "  \\  /" , "  \\ / ", "   0", "  / | \\", "  /  \\"
+        
+        };  
+
+        public void PrintGuy()
+        {
+            foreach (string i in parachute)
             {
-                parachute.Add(i.ToString());
+                Console.WriteLine(i);
             }
-            return parachute;
 
             
         }
     }
 
-    static void PrintGuy(List<string> parachute)
-    {
-        Console.WriteLine();
-        Console.WriteLine($" {parachute[0]}____");
-        Console.WriteLine($" /{parachute[1]}   \\ " );
-        Console.WriteLine($" {parachute[2]}_____");
-        Console.WriteLine($"  \\{parachute[3]}  /" );
-        Console.WriteLine($"   \\{parachute[4]}/ " );
-        Console.WriteLine($" {parachute[5]}  0" );
-        Console.WriteLine($"  / | \\" );
-        Console.WriteLine($"   /  \\" );
-
-
-
-    }
-    
-}
+   
 }
