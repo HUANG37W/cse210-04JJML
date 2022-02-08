c3ddce4b082e99c389fba791d4defe4f6faf14cf
jnounna <97972572+jnounna@users.noreply.github.com>
2022-02-08 09:58:49 -0700

    finishing update

--- unit03-jumper/game/SkyGuy.cs
+++ unit03-jumper/game/SkyGuy.cs
@@ -23,26 +23,5 @@ namespace unit03_jumper
         }
     }
 
-<<<<<<< HEAD
-    static void PrintGuy(List<string> parachute)
-    {
-        Console.WriteLine();
-        Console.WriteLine($" {parachute[0]}____");
-        Console.WriteLine($" /{parachute[1]}   \\ " );
-        Console.WriteLine($" {parachute[2]}_____");
-        Console.WriteLine($"  \\{parachute[3]}  /" );
-        Console.WriteLine($"   \\{parachute[4]}/ " );
-        Console.WriteLine($" {parachute[5]}  0" );
-        Console.WriteLine($"  / | \\" );
-        Console.WriteLine($"   /  \\" );
 
-
-
-    }
-
-    
-    
-=======
-   
->>>>>>> 3380698fee0f3d00b69045c13c3ecc5d77ede4c1
 }
