0a27806cde8e9ce15515817bfc12db5d4f498e66
Michael Knudsen <mikeman825@gmail.com>
2022-02-18 22:17:52 -0700

    Skydrops replaced w/ sub classes: Gem and Stone

---
+++ unit04-greed/Game/Casting/Gem.cs
@@ -0,0 +1,10 @@
+using System;
+namespace Unit04.Game.Casting
+{
+    class Gem : Actor
+    {
+        //falling object
+        //looks like a "*"
+        //scoreboard increases in points when this is caught by player
+    }
+}
