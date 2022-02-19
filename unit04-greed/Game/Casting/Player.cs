f1ffffbb205b9223c2f440e6a2304ff74f9947af
Michael Knudsen <mikeman825@gmail.com>
2022-02-18 22:17:09 -0700

    Skydrops replaced w/ two sub classes: Stone & Gem

--- unit04-greed/Game/Casting/Player.cs
+++ unit04-greed/Game/Casting/Player.cs
@@ -7,7 +7,7 @@ namespace Unit04.Game.Casting
     public class Player : Actor
     {
         //follow how Robot works from previous asssignment
-
+        
 
     }
 
---
+++ unit04-greed/Game/Casting/Stone.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Unit04.Game.Casting
+{
+    class Stone : Actor
+    {
+        //falling object.
+        //looks like a square
+        //removes points when caught
+
+    }
+}
