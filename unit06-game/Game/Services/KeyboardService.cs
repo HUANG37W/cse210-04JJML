fa529d232b0f3b3d0d355867c56694a239ea2fdc
Lisa Child <gustavus05@gmail.com>
2022-03-21 20:57:05 -0600

    changed sceneaction and keyboard services, actioncallback

--- unit06-game/Game/Services/KeyboardService.cs
+++ unit06-game/Game/Services/KeyboardService.cs
@@ -5,61 +5,92 @@ using Unit06.Game.Casting;
 
 namespace Unit06.Game.Services
 {
-    /// <summary>
-    /// <para>Detects player input.</para>
-    /// <para>
-    /// The responsibility of a KeyboardService is to indicate whether or not a key is up or down.
-    /// </para>
-    /// </summary>
-    public class KeyboardService
+    public interface KeyboardService
     {
-        private Dictionary<string, KeyboardKey> keys
-                = new Dictionary<string, KeyboardKey>();
+        /// <summary>
+        /// Whether or not the given key is down.
+        /// </summary>
+        /// <param name="key">The given key.</param>
+        bool IsKeyDown(string key);
 
         /// <summary>
-        /// Constructs a new instance of KeyboardService using the given cell size.
+        /// Whether or not the given key has been pressed.
         /// </summary>
-        public KeyboardService()
-        {
-            //player 1 key definitions
-            keys["w"] = KeyboardKey.KEY_W;
-            keys["a"] = KeyboardKey.KEY_A;
-            keys["s"] = KeyboardKey.KEY_S;
-            keys["d"] = KeyboardKey.KEY_D;
-            keys["e"] = KeyboardKey.KEY_E;
-            
-            //player 2 key definitions
-            keys["i"] = KeyboardKey.KEY_I;
-            keys["j"] = KeyboardKey.KEY_J;
-            keys["k"] = KeyboardKey.KEY_K;
-            keys["l"] = KeyboardKey.KEY_L;
-            keys["u"] = KeyboardKey.KEY_U;
-
-            
-            
-        }
+        /// <param name="key">The given key.</param>
+        bool IsKeyPressed(string key);
 
         /// <summary>
-        /// Checks if the given key is currently down.
+        /// Whether or not the given key has been released.
         /// </summary>
-        /// <param name="key">The given key (w, a, s, d, i, j, k, or l)</param>
-        /// <returns>True if the given key is down; false if otherwise.</returns>
-        public bool IsKeyDown(string key)
-        {
-            KeyboardKey raylibKey = keys[key.ToLower()];
-            return Raylib.IsKeyDown(raylibKey);
-        }
+        /// <param name="key">The given key.</param>
+        bool IsKeyReleased(string key);
 
         /// <summary>
-        /// Checks if the given key is currently up.
+        /// Whether or not the given key is up.
         /// </summary>
-        /// <param name="key">The given key (w, a, s, d, i, j, k, l, e, or u)</param>
-        /// <returns>True if the given key is up; false if otherwise.</returns>
-        public bool IsKeyUp(string key)
-        {
-            KeyboardKey raylibKey = keys[key.ToLower()];
-            return Raylib.IsKeyUp(raylibKey);
-        }
-
+        /// <param name="key">The given key.</param>
+        bool IsKeyUp(string key);
     }
 }
+
+
+
+// {
+//     /// <summary>
+//     /// <para>Detects player input.</para>
+//     /// <para>
+//     /// The responsibility of a KeyboardService is to indicate whether or not a key is up or down.
+//     /// </para>
+//     /// </summary>
+//     public class KeyboardService
+//     {
+//         private Dictionary<string, KeyboardKey> keys
+//                 = new Dictionary<string, KeyboardKey>();
+
+//         /// <summary>
+//         /// Constructs a new instance of KeyboardService using the given cell size.
+//         /// </summary>
+//         public KeyboardService()
+//         {
+//             //player 1 key definitions
+//             keys["w"] = KeyboardKey.KEY_W;
+//             keys["a"] = KeyboardKey.KEY_A;
+//             keys["s"] = KeyboardKey.KEY_S;
+//             keys["d"] = KeyboardKey.KEY_D;
+//             keys["e"] = KeyboardKey.KEY_E;
+            
+//             //player 2 key definitions
+//             keys["i"] = KeyboardKey.KEY_I;
+//             keys["j"] = KeyboardKey.KEY_J;
+//             keys["k"] = KeyboardKey.KEY_K;
+//             keys["l"] = KeyboardKey.KEY_L;
+//             keys["u"] = KeyboardKey.KEY_U;
+
+            
+            
+//         }
+
+//         /// <summary>
+//         /// Checks if the given key is currently down.
+//         /// </summary>
+//         /// <param name="key">The given key (w, a, s, d, i, j, k, or l)</param>
+//         /// <returns>True if the given key is down; false if otherwise.</returns>
+//         public bool IsKeyDown(string key)
+//         {
+//             KeyboardKey raylibKey = keys[key.ToLower()];
+//             return Raylib.IsKeyDown(raylibKey);
+//         }
+
+//         /// <summary>
+//         /// Checks if the given key is currently up.
+//         /// </summary>
+//         /// <param name="key">The given key (w, a, s, d, i, j, k, l, e, or u)</param>
+//         /// <returns>True if the given key is up; false if otherwise.</returns>
+//         public bool IsKeyUp(string key)
+//         {
+//             KeyboardKey raylibKey = keys[key.ToLower()];
+//             return Raylib.IsKeyUp(raylibKey);
+//         }
+
+//     }
+// }
