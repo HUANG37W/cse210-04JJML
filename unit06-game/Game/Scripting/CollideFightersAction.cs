c9d3ec2ea9e953bd3beec876b64ccd303f03126e
Lisa Child <gustavus05@gmail.com>
2022-03-29 09:05:40 -0600

    added system in collidefighters and sound in constants

--- unit06-game/Game/Scripting/CollideFightersAction.cs
+++ unit06-game/Game/Scripting/CollideFightersAction.cs
@@ -1,5 +1,6 @@
 using Unit06.Game.Casting;
 using Unit06.Game.Services;
+using System.Collections.Generic;
 
 namespace Unit06.Game.Scripting
 {
@@ -20,6 +21,25 @@ namespace Unit06.Game.Scripting
 
         public void Execute(Cast cast, Script script, ActionCallback actionCallback)
         {
+            List<Actor> bowfighters = cast.GetActors(Constants.BOW_FIGHTER_GROUP);
+            List<Actor> bears = cast.GetActors(Constants.BEAR_GROUP);
+            List<Actor> swordfighters = cast.GetActors(Constants.FIGHTER_GROUP);
+
+            Stats stats = (Stats)cast.GetFirstActor(Constants.STATS_GROUP);
+
+            
+            // Body bearbody = bears.GetBody();
+            // Body swordfightersbody = swordfighters.GetBody();
+            // if (physicsService.HasCollided(bearbody,swordfightersbody))
+            // {
+            //     Sound sound = new Sound(Constants.BEAR_GROWL_SOUND);
+            //     audioService.PlaySound(sound);
+            //     int points = bears.GetPoints();
+            //     stats.AddPoints(points);
+            //     cast.RemoveActor(Constants.FIGHTER_GROUP, swordfighters);
+            // }
+
+            
             //maybe try a for each loop to loop through every fighter that could collide with one another.
         }
     }
