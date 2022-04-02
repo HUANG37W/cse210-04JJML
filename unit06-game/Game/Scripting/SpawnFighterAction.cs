78b8d25818ad7554976326e36772ad7b8d79d84b
Lisa Child <gustavus05@gmail.com>
2022-04-01 18:25:22 -0600

    bowman fixed

--- unit06-game/Game/Scripting/SpawnFighterAction.cs
+++ unit06-game/Game/Scripting/SpawnFighterAction.cs
@@ -64,33 +64,15 @@ namespace Unit06.Game.Scripting
 
             else if (keyboardService.IsKeyPressed("c"))
             {
-                //sets the pixel size and velocity values
-                Point size = new Point(Constants.BOW_FIGHTER_WIDTH, Constants.BOW_FIGHTER_HEIGHT);
-                Point velocity = new Point(Constants.BOW_FIGHTER_VELOCITY, 0);
-
-
-                //sets the point sp1, size, and velocity to the fighter
-                Body BowFighterBody = new Body(sp1, size, velocity);
-                Animation animation = new Animation(Constants.BOW_FIGHTER_IMAGES, 0, Constants.FIGHTER_RATE);
-                BowFighter bf = new BowFighter(BowFighterBody, animation);
-                cast.AddActor(Constants.FIGHTER_GROUP, bf); //adds bowfighter f to the cast
-                // Make the sound
+                BowFighter bwf = new BowFighter(sp1, 1, false);
+                cast.AddActor(Constants.FIGHTER_GROUP, bwf); //adds bowfighter swf to the cast
             }
 
             else if (keyboardService.IsKeyPressed("n"))
             {
-                //sets the pixel size and velocity values to size and velocity
-                Point size = new Point(Constants.BOW_FIGHTER_WIDTH, Constants.BOW_FIGHTER_HEIGHT);
-                Point velocity = new Point(Constants.BOW_FIGHTER2_VELOCITY, 0);
-
-
-
-                //sets the point sp2, size, and velocity to the player2 fighter
-                Body BowFighterBody = new Body(sp2, size, velocity);
-                Animation animation = new Animation(Constants.BOW_FIGHTER2_IMAGES, 0, Constants.FIGHTER_RATE);
-                BowFighter bf = new BowFighter(BowFighterBody, animation);
-                cast.AddActor(Constants.FIGHTER_GROUP, bf); //adds fighter f to the cast
-                // Make the sound
+                BowFighter bwf = new BowFighter(sp2, 2, false);
+                cast.AddActor(Constants.FIGHTER_GROUP, bwf); //adds swordfighter swf to the cast
+                
             }
         }
 
